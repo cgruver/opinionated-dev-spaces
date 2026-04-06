39a53082416129d3c20b4323a0f1850e7c3e406a
cgruver <cgruver@redhat.com>
2026-04-06 19:00:32 +0000

    add configs for Maven through Nexus
    Signed-off-by: cgruver <cgruver@redhat.com>

--- cekit-image-library/modules/dev-base/entrypoint.sh
+++ cekit-image-library/modules/dev-base/entrypoint.sh
@@ -17,17 +17,41 @@ if [ ! -d "${HOME}/.config/containers" ]; then
   fi
 fi
 
+# Create Java Keystore
+if [ command -v keytool > /dev/null 2>&1 ] & [ ! -f ${HOME}/.keystore ]
+then
+  CA_BUNDLE="${JAVA_CA_BUNDLE:-/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem}"
+  KEYSTORE_PASSWORD="${JAVA_KEYSTORE_PASSWORD:-changeit}"
+  TEMP_DIR="$(mktemp -d)"
+  pushd ${TEMP_DIR}
+  cat ${CA_BUNDLE} | grep -v "#" > tmpfile
+  csplit -z -f crt- ./tmpfile '/-----BEGIN CERTIFICATE-----/' '{*}'
+  for cert in crt-*
+  do
+    keytool -import -noprompt -file $cert -storepass ${KEYSTORE_PASSWORD} -alias service-$cert
+  done
+  popd
+fi
+
 # Configure Z shell
 if [ ! -f ${HOME}/.zshrc ]
 then
   (echo "HISTFILE=${HOME}/.zsh_history"; echo "HISTSIZE=1000"; echo "SAVEHIST=1000") > ${HOME}/.zshrc
   (echo "if [ -f ${PROJECT_SOURCE}/workspace.rc ]"; echo "then"; echo "  . ${PROJECT_SOURCE}/workspace.rc"; echo "fi") >> ${HOME}/.zshrc
+  if [ -f ${HOME}/.keystore ]
+  then
+    echo "export MAVEN_OPTS=\"-Djavax.net.ssl.trustStore=${HOME}/.keystore -Djavax.net.ssl.trustStorePassword=changeit\"" >> ${HOME}/.zshrc
+  fi
 fi
 
 # Configure Bash shell
 if [ ! -f ${HOME}/.bashrc ]
 then
   (echo "if [ -f ${PROJECT_SOURCE}/workspace.rc ]"; echo "then"; echo "  . ${PROJECT_SOURCE}/workspace.rc"; echo "fi") > ${HOME}/.bashrc
+  if [ -f ${HOME}/.keystore ]
+  then
+    echo "export MAVEN_OPTS=\"-Djavax.net.ssl.trustStore=${HOME}/.keystore -Djavax.net.ssl.trustStorePassword=changeit\"" >> ${HOME}/.zshrc
+  fi
 fi
 
 exec "$@"
