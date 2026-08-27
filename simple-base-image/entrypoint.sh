cb3ec531c85dd78d9c1f6dc3a6a05309d18d6548
cgruver <cgruver@redhat.com>
2026-08-27 11:28:40 +0000

    modified to dynamically set UID namespace size

--- simple-base-image/entrypoint.sh
+++ simple-base-image/entrypoint.sh
@@ -13,4 +13,12 @@ then
   (echo '[storage]';echo 'driver = "overlay"';echo 'graphroot = "/tmp/graphroot"';echo '[storage.options.overlay]';echo 'mount_program = "/usr/bin/fuse-overlayfs"') > ${HOME}/.config/containers/storage.conf
 fi
 
+# Set up UID and GID mappings for the user
+CURRENT_UID=$(id -u)
+START_ID=$(( CURRENT_UID + 1 ))
+NAMESPACE_SIZE=$(awk '{end=$1 + $3; if (end > max) max = end} END{print max}' /proc/self/uid_map)
+SUB_ID_COUNT=$(( NAMESPACE_SIZE - START_ID ))
+echo "$(whoami):${START_ID}:${SUB_ID_COUNT}" > /etc/subuid
+echo "$(whoami):${START_ID}:${SUB_ID_COUNT}" > /etc/subgid
+
 exec "$@"
