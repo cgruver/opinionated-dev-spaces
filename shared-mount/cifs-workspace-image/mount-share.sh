82999bba119b819c869caad7abb3a01d74c02cce
cgruver <cgruver@redhat.com>
2026-09-24 11:56:46 +0000

    update

--- shared-mount/cifs-workspace-image/mount-share.sh
+++ shared-mount/cifs-workspace-image/mount-share.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-id
-whoami
+USER_PWD="hello"
+USER_PWD_CHK="goodbye"
+echo "Enter the URL for the CIFS share:"
+read CIFS_URL
+echo "Enter the mountpoint for the CIFS share:"
+read CIFS_MNT
+echo "Enter the user id for the CIFS mount:"
+read CIFS_USER
+while [[ ${USER_PWD} != ${USER_PWD_CHK} ]]
+do
+  echo "Enter the password for the CIFS mount:"
+  read -s USER_PWD
+  echo "Re-Enter the password for the CIFS mount:"
+  read -s USER_PWD_CHK
+  if [[ ${USER_PWD} != ${USER_PWD_CHK} ]]
+  then
+    echo "Passwords do not match. Try Again."
+  fi
+done
 
-mount -t cifs -o username=your_smb_user,password=password,uid=$(id -u),gid=$(id -g) //192.168.1.50/SharedFolder /mnt/my_share
+if [[ ! -d ${CIFS_MNT} ]]
+then
+  mkdir -p ${CIFS_MNT}
+fi
+mount -t cifs -o username=${CIFS_USER},password=${USER_PWD},uid=$(id -u),gid=$(id -g) ${CIFS_URL} ${CIFS_MNT}
